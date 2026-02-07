b172dae523dedb6b9e317ab24308ba7cd649f335
Ololade <73138104+zololade@users.noreply.github.com>
2026-02-07 10:48:51 +0100

    fix: add dialog css

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,5 @@ import "./script/UI/Icons";
 import "./script/UI/gameBoard";
 import "./script/UI/UiGame";
 
-let dialog = document.querySelector(".gameOption") as HTMLDialogElement;
-dialog.showModal();
+// let dialog = document.querySelector(".gameOption") as HTMLDialogElement;
+// dialog.showModal();
